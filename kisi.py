b1593be2b6c2db3a78204ffc7809d5e8106f7f7e
Haflidi Haflidason <haflidih20@ru.is>
2020-09-03 10:47:46 +0000

    Changed kisi.py

---
+++ kisi.py
@@ -0,0 +1,5 @@
+# Collect numbers from the input and sort them to find the largest integer.
+# If the number is negative, then it won´t go through the algorithm.
+#
+# The algorithm takes a number and values if it´s negative or not, if it´s not negative
+# then the algorithm checks if it´s larger than max_int and replaces max_int if so.
